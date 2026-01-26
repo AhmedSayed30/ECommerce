b47f721e2a2c8d354c409383d37763f130ed76c0
AhmedSayed30 <as3461074@gmail.com>
2026-01-26 23:01:50 +0200

    +add register fragment

--- app/src/main/java/com/training/ecommerce/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/training/ecommerce/ui/auth/AuthActivity.kt
@@ -1,4 +1,4 @@
-package com.training.ecommerce.ui.auth.login
+package com.training.ecommerce.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
---
+++ app/src/main/java/com/training/ecommerce/ui/auth/register/fragment/RegisterFragment.kt
@@ -0,0 +1,34 @@
+package com.training.ecommerce.ui.auth.register.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.training.ecommerce.R
+import com.training.ecommerce.databinding.FragmentRegisterBinding
+import com.training.ecommerce.ui.home.MainActivity
+
+
+class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding=FragmentRegisterBinding.inflate(inflater,container,false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvLogin.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+}
